c65d142e6a494cfab1325c51b467a6888e024938
Quentin <qlaffont13@gmail.com>
2023-12-28 06:54:01 +0100

    feat(app): add vidstack

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,18 @@ import {createEffect} from "solid-js";
 
 import bg from '../../public/test.jpeg';
 
+
+import 'vidstack/player/styles/default/theme.css';
+import 'vidstack/player/styles/default/layouts/video.css';
+
+
+import 'vidstack/player';
+import 'vidstack/player/layouts';
+import 'vidstack/player/ui';
+
+
+
+
 export default function Home() {
   // const i18n=useRosetty();
 
@@ -15,6 +27,14 @@ export default function Home() {
     <main class="text-center mx-auto text-gray-700 p-4">
       <img src="/cursor.svg" />
       <img src={bg} />
+
+    YOUTUBE VIDEO: 
+      <media-player title="Sprite Fight" src="youtube/_cMxraX_5RE">
+        <media-provider></media-provider>
+        <media-video-layout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"></media-video-layout>
+      </media-player>
+
+
       {/* <button class="border" onClick={() => i18n.changeLang('en')}>EN</button>
       <button class="border" onClick={() => i18n.changeLang('fr')}>fr</button>
       {i18n.t('test')} */}
